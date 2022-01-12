da3948e26cc0a267dc4a691a4401a14a10544b75
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-01-12 08:38:44 +0100

    Add code print result

---
+++ index.js
@@ -0,0 +1,51 @@
+import kuler from 'kuler';
+
+import randomColor from 'randomcolor';
+
+// Prints result random:
+function printResult(color) {
+  let str = '#';
+  let space = ' ';
+  let fullLine = str.repeat(31);
+  let partLine = str.repeat(5);
+  let partSpace = space.repeat(5);
+  let partSpace2 = space.repeat(7);
+
+  console.log(kuler(fullLine, color));
+  console.log(kuler(fullLine, color));
+  console.log(kuler(fullLine, color));
+  console.log(
+    kuler(
+      `${partLine} ${partSpace} ${partSpace2} ${partSpace} ${partLine}`,
+      color,
+    ),
+  );
+  console.log(
+    kuler(`${partLine} ${partSpace} ${color} ${partSpace} ${partLine}`, color),
+  );
+  console.log(
+    kuler(
+      `${partLine} ${partSpace} ${partSpace2} ${partSpace} ${partLine}`,
+      color,
+    ),
+  );
+  console.log(kuler(fullLine, color));
+  console.log(kuler(fullLine, color));
+  console.log(kuler(fullLine, color));
+}
+
+// Randomly generates color
+
+let color;
+if (process.argv[2]) {
+  if (process.argv[3]) {
+    color = randomColor({ luminosity: process.argv[3], hue: process.argv[2] });
+    printResult(color);
+  } else {
+    color = randomColor({ hue: process.argv[2] });
+    printResult(color);
+  }
+} else {
+  color = randomColor();
+  printResult(color);
+}
